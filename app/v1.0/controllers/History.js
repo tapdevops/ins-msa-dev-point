9eb7494422bac50bec95af3e8608868096f4da72
ihsanhusaeri <ihusaeri@gmail.com>
2020-10-01 13:57:05 +0700

    ubah format report

--- app/v1.0/controllers/History.js
+++ app/v1.0/controllers/History.js
@@ -6,11 +6,12 @@
  | Untuk menghandle models, libraries, helper, node modules, dan lain-lain
  |
  */
-    
+    //Models
+    const Est = require(_directory_base + '/app/v1.0/models/Est.js');
     const History = require( _directory_base + '/app/v1.0/models/History.js' );
-    const dateformat = require('dateformat');
     
     //Node_modules
+    const dateformat = require('dateformat');
     const async = require('async');
 
     exports.report = async (req, res) => {
@@ -18,8 +19,40 @@
         // try {
         async.auto({
             getAllHistory: function(callback) {
-                History.find({ PERIOD: month }).select({ _id: 0 })
+                History.aggregate([
+                    {
+                        $lookup: {
+                            from: "VIEW_USER_AUTH",
+                            foreignField: "USER_AUTH_CODE",
+                            localField: "USER_AUTH_CODE",
+                            as: "viewUser"
+                        }
+                    },{
+                        $unwind: "$viewUser" 
+                    }, {
+                        $project: {
+                            _id: 0,
+                            FULLNAME: {$ifNull: ["$viewUser.HRIS_FULLNAME", "$viewUser.PJS_FULLNAME"]},
+                            NIK: "$viewUser.EMPLOYEE_NIK",
+                            JOB: "$viewUser.USER_ROLE",
+                            BA_CODE: 1,
+                            POINT: 1,
+                            USER_AUTH_CODE : 1,
+                            PERIOD: 1,
+                            DATE: 1,
+                            TYPE: 1,
+                            REMARKS: 1,
+                            REFERENCE: 1
+                        }
+                    }, {
+                        $match: {
+                            PERIOD: parseInt(month),
+                            JOB: 'ASISTEN_LAPANGAN'
+                        } 
+                    }
+                ])
                 .then(data => {
+                    // console.log(data);
                     callback(null, data);
                 })
                 .catch(err => {
@@ -27,7 +60,7 @@
                     callback(err, null);
                 });
             }, 
-            mappingData: ['getAllHistory', function(results,  callback) {
+            mappingPeriode: ['getAllHistory', function(results,  callback) {
                 let histories = results.getAllHistory;
                 histories.map(async (history) => {
                     let date = history.DATE.toString();
@@ -39,6 +72,27 @@
                     history.DATE = periode;
                 });
                 callback(null, histories)
+            }],
+            getEstateName: ['mappingPeriode', function(results, callback) {
+                let histories = results.mappingPeriode;
+                async.each(histories, function(history, callbackEach) {
+                    Est.findOne({WERKS: history.BA_CODE}).select({_id: 0, EST_NAME: 1})
+                    .then( data => {
+                        history.BUSINESS_AREA = data.EST_NAME;
+                        callbackEach();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        callbackEach(err, null);
+                    });
+                }, function(err) {
+                    if (err) {
+                        callback(err, null);
+                        return;
+                    } else {
+                        callback(null, histories);
+                    }
+                })     
             }]
         }, function(err, results) {
             if(err) {
@@ -48,11 +102,11 @@
                     data: []
                 })
             }
-            console.log(results.mappingData)
+            // console.log(results.mappingData)
             res.send({
                 status: true,
                 message: 'Success',
-                data: results.mappingData
+                data: results.getEstateName
             });
         })
         
